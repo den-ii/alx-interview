b0269c323a418484a41edb03d4dae40d1a88127a
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-05-18 22:25:12 +0100

    added main.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,27 @@
-# 0x04. UTF-8 Validation
-```0. UTF-8 Validation        mandatory````
-Write a method that determines if a given data set represents a valid UTF-8 encoding.
-
-Prototype: def validUTF8(data)
-Return: True if data is a valid UTF-8 encoding, else return False
-A character in UTF-8 can be 1 to 4 bytes long
-The data set can contain multiple characters
-The data will be represented by a list of integers
-Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
-
-```
-carrie@ubuntu:~/0x04-utf8_validation$ cat 0-main.py
 #!/usr/bin/python3
-"""
-Main file for testing
-"""
+""" 0-validae_utf8.py """
 
-validUTF8 = __import__('0-validate_utf8').validUTF8
 
-data = [65]
-print(validUTF8(data))
+def validUTF8(data):
+    """"
+    Determines if the data represents a valid UTF-8 encoding.
 
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
+    Args:
+        data: a list of integers representing encoding
 
-data = [229, 65, 127, 256]
-print(validUTF8(data))
-```
+    Returns:
+        True if the data is a valid UTF-8 ecoding otherwise False.
+    """
+    if not data:
+        return False
 
-```
-carrie@ubuntu:~/0x04-utf8_validation$
-carrie@ubuntu:~/0x04-utf8_validation$ ./0-main.py
-True
-True
-False
-carrie@ubuntu:~/0x04-utf8_validation$
-```
+    if data[0] & 0x80 == 0:
+        return True
+
+    if data[0] & 0xC0 == 0xC0:
+        return False
+
+    for i in range(1, len(data)):
+        if data[i] & 0xC0 != 0x80:
+            return False
+        return True
