0719198e28e94dbb0c7f9a0c2a9fdc2a8ba40766
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-05-31 19:58:29 +0100

    first commit for starwars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+let characters;
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  async function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    const newBody = await body;
+    characters = JSON.parse(newBody).characters;
+    characters.forEach((element) => {
+      request(element, async function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        const newBody = await body;
+        console.log(JSON.parse(newBody).name);
+      });
+    });
+  }
+);
