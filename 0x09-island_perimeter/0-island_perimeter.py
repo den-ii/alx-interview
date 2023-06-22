be05dca3119952b9319e0b89e8bbb7c7adcd550e
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-06-22 20:46:16 +0100

    0x09

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,31 @@
 """
     0-island_perimeter module
-""""
+"""
+
 
 def island_perimeter(grid):
     """
      arg(grid): a list of integers
      Returns: perimeter of island
     """
-    
+
     if not grid or not grid[0]:
         return 0
-    
+
     rows = len(grid)
     cols = len(grid[0])
-    
+
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4  # Each land cell contributes 4 sides to the perimeter
-                
+
                 # Check if neighboring cells are also land
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2  # Subtract 2 for each adjacent land cell
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2  # Subtract 2 for each adjacent land cell
-    
+
     return perimeter
