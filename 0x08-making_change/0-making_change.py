af78448c4a4edc655e490030f573edef09c84d9d
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-06-22 20:50:22 +0100

    0x09

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,10 +6,7 @@ def makeChange(coins, total):
     '''
      coin(args): List
      total(args): int
-     Returns: 
-     0 | -1 | no of coins to total
     '''
-
     if total <= 0:
         return 0
 
@@ -21,7 +18,9 @@ def makeChange(coins, total):
     for coin in coins:
         # Update the min_coins list for each amount from coin to total
         for amount in range(coin, total + 1):
-            # Choose the minimum between the current min_coins[amount] and min_coins[amount - coin] + 1
+            # Choose the minimum between the
+            # current min_coins[amount]
+            # and min_coins[amount - coin] + 1
             min_coins[amount] = min(
                 min_coins[amount], min_coins[amount - coin] + 1)
 
@@ -40,16 +39,19 @@ def makeChange(coins, total):
     # # returns 0 if total is <= 0
     # if (total <= 0):
     #     return 0
-    # # if coins length is 1 that me we have only 1 coins we check if it is decimal or int
+    # if coins length is 1 that me
+    # we have only 1 coins we check
+    # if it is decimal or int
     # elif (len(coins) == 1):
     #     if total/coins is type(bool) or coins[0] < total:
     #         return -1
     #     return total/coins
 
-    # # while coinStack is not equal to total
+    #  while coinStack is not equal to total
     # while coinsStack != total:
     #     # print("loop")
-    #     # check if coinstack + coins1[0] < total; add it; increase needed coins
+    # check if coinstack + coins1[0] < total;
+    # add it; increase needed coins
     #     if coinsStack + coins1[0] <= total:
     #         coinsStack += coins1[0]
     #         neededCoins += 1
@@ -62,7 +64,8 @@ def makeChange(coins, total):
     #             # elif len(coins1) > 2:
     #             coins1 = coins1[1:]
     #     # pop the list again to form a new one
-    #     elif coinsStack + coins1[0] > total and len(coins1) >= 1 and coins2 >= 2:
+    #     elif coinsStack + coins1[0] > total and
+    # len(coins1) >= 1 and coins2 >= 2:
     #         # print("3rd if")
     #         coins1 = coins[1:]
     #         coins2 = coins2 - 1
@@ -75,6 +78,4 @@ def makeChange(coins, total):
     #         # break
     #         return -1
     # return neededCoins
-
-
 # print(makeChange([1, 2, 25], 37))
